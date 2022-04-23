10bb20106cfcc16bca3deed173a111c4244b92af
callumhemming <callum.m.hemming@outlook.com>
2022-04-23 22:11:49 +0100

    Made some changes in newFolder/main.js

--- newFolder/main.js
+++ newFolder/main.js
@@ -1,14 +1,19 @@
-while(true){
+// while(true){
     
-    const Response = prompt ("Callum smells");
-console.log (Response);
-if (Response === "no"){
-    prompt ("WRONG ANSWER")
-}
-if (Response === "yes"){
-    prompt ("I KNOW RIGHT")
-}else{alert ("Write something ass hole")}
+//     const Response = prompt ("Callum smells");
+// console.log (Response);
+// if (Response === "no"){
+//     prompt ("WRONG ANSWER")
+// }
+// if (Response === "yes"){
+//     prompt ("I KNOW RIGHT")
+// }else{alert ("Write something ass hole")}
 
 
 
-}
+// }
+
+
+
+const nameOfBestBoyFriend = "Callum"
+alert(`${nameOfBestBoyFriend} is the best boyfriend ever!`)
