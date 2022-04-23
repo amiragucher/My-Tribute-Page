d3f5abd51cf04e75953823600add0ae435a933ab
callumhemming <callum.m.hemming@outlook.com>
2022-04-23 22:20:51 +0100

    Added a break to the dangerous while loop

--- newFolder/main.js
+++ newFolder/main.js
@@ -1,16 +1,17 @@
- while(true){
-   
-     const Response = prompt ("Callum is a good guy");
- console.log (Response);
- if (Response === "no"){     prompt ("WRONG ANSWER")
- }
- if (Response === "yes"){
-     prompt ("I KNOW RIGHT")
- }else{alert ("Write something ass hole")}
 
+const callumHasBeenInsulted = false;
+while (!callumHasBeenInsulted) {
+  const Response = prompt("Callum is a good guy");
+  console.log(Response);
+  if (Response === "no") {
+    prompt("WRONG ANSWER");
+  }else if (Response === "yes") {
+    prompt("I KNOW RIGHT");
+    callumHasBeenInsulted = true;
+  } else {
+    alert("Write something ass hole");
+  }
+}
 
-
-
-
-const nameOfBestBoyFriend = "Callum"
-alert(`${nameOfBestBoyFriend} is the best boyfriend ever!`)
+const nameOfBestBoyFriend = "Callum";
+alert(`${nameOfBestBoyFriend} is the best boyfriend ever!`);
